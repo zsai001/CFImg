021621605d8be0bdb2f0cefd78df135cfeeb9617
zsai <zsai@tsiannian.com>
2024-05-02 22:09:24 +0800

    hello

--- functions/upload.js
+++ functions/upload.js
@@ -1,19 +1,19 @@
-export async function onRequestPost(context) {  // Contents of context object  
-    const {   
-        request, // same as existing Worker API    
-    env, // same as existing Worker API    
-    params, // if filename includes [id] or [[path]]   
-     waitUntil, // same as ctx.waitUntil in existing Worker API    
-     next, // used for middleware or to fetch assets    
-     data, // arbitrary space for passing data between middlewares 
+export async function onRequestPost(context) {  // Contents of context object
+    const {
+        request, // same as existing Worker API
+    env, // same as existing Worker API
+    params, // if filename includes [id] or [[path]]
+     waitUntil, // same as ctx.waitUntil in existing Worker API
+     next, // used for middleware or to fetch assets
+     data, // arbitrary space for passing data between middlewares
      } = context;
      context.request
+    let back = env.BackEnd;
      const url = new URL(request.url);
-     const response = fetch('https://telegra.ph/' + url.pathname + url.search, {
+     const response = fetch('https://' + back + '/' + url.pathname + url.search, {
          method: request.method,
          headers: request.headers,
          body: request.body,
      });
     return response;
   }
-  
